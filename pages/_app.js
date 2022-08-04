9ff6f74a22cae956beeebe38bf38d433ef666192
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-08-04 21:05:21 +0530

    feat(redux): setup redux

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,20 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 import { CSSReset } from "@chakra-ui/react";
 // 1. Import the extendTheme function
 import theme from "../lib/theme";
 import { Chakra } from "../lib/Chakra";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 
 function MyApp({ Component, pageProps }) {
-	return (
-		<Chakra cookies={pageProps.cookies} theme={theme} cssVarsRoot="body">
-			<CSSReset />
-			<Component {...pageProps} />
-		</Chakra>
-	);
+  return (
+    <Provider store={store}>
+      <Chakra cookies={pageProps.cookies} theme={theme} cssVarsRoot="body">
+        <CSSReset />
+        <Component {...pageProps} />
+      </Chakra>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
