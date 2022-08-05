065e8651a839712577abf4389049d721ff07ee12
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2022-08-05 18:43:56 +0530

    Merge fix

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,17 +2,21 @@ import "../styles/globals.css";
 import { CSSReset } from "@chakra-ui/react";
 import { Chakra } from "../shared/lib/chakra";
 import theme from "../shared/lib/theme";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Chakra
-      cookies={pageProps.cookies}
-      theme={theme}
-      cssVarsRoot="body"
-    >
-      <CSSReset />
-      <Component {...pageProps} />
-    </Chakra>
+    <Provider store={store}>
+      <Chakra
+        cookies={pageProps.cookies}
+        theme={theme}
+        cssVarsRoot="body"
+      >
+        <CSSReset />
+        <Component {...pageProps} />
+      </Chakra>
+    </Provider>
   );
 }
 
