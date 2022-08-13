0a0c4937bf9fa867b76c2608ab0bd129b166ca17
Devansh Bajaj <devansh21db.db@gmail.com>
2022-08-13 21:18:45 +0530

    feat(courses): add course page

--- dashboard/components/index.jsx
+++ dashboard/components/index.jsx
@@ -1,3 +1,5 @@
 export { default as Sidebar } from "./sidebar";
 export { default as HomeCard } from "./home/home-card";
 export { default as Layout } from "./layout";
+export { default as CourseCard } from "./course/course-card";
+export { default as CourseModal } from "./course/course-modal";
