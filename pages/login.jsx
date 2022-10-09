221aea95748dfcc50c982b74f4ccf43f4268f5d8
Devansh Bajaj <devansh21db.db@gmail.com>
2022-10-09 16:10:40 +0530

    fix: login state

--- pages/login.jsx
+++ pages/login.jsx
@@ -39,7 +39,7 @@ export default function Login() {
   const { register, handleSubmit, formState: { errors } } = useForm();
 
   useEffect(() => {
-    if (token !== "") {
+    if (token !== "" && token !== undefined) {
       router.push("/dashboard", undefined, { shallow: true });
     }
   }, [token]);
